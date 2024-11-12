986b321b8214d5e52264f2e257593c410b541a46
Dimas Zikri <dschok182@gmail.com>
2024-11-12 14:10:47 +0700

    Initial commit

---
+++ src/app/selfcustom/selfcustom-routing.module.ts
@@ -0,0 +1,109 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { EmployeeDetailSelfcustomComponent } from "./employee/employee-add/employee-detail-selfcustom.component";
+import { PathConstantSelfCustom } from "./shared/constant/PathConstantSelfCustom";
+import { UcTemplateComponent } from "@adins/uctemplate";
+
+const routes: Routes = [
+  {
+    path: "",
+    children: [
+      {
+        path: PathConstantSelfCustom.EMP_PAGING_CNAF,
+        // component: EmployeeDetailSelfcustomComponent,
+        component: UcTemplateComponent,
+        data: {
+          title: "Employee Paging SelfCustom",
+          page: "EmployeePagingXCNAF",
+        },
+      },
+      {
+        path: PathConstantSelfCustom.EMP_DETAIL_CNAF,
+        // component: EmployeeDetailSelfcustomComponent,
+        component: UcTemplateComponent,
+        data: {
+          title: "Employee Detail SelfCustom",
+          page: "OrganizationemployeedetailCopyXCNAFSelfCustomBriliyan",
+        },
+      },
+      {
+        path: PathConstantSelfCustom.REIMB_REQ_CNAF,
+        // component: EmployeeDetailSelfcustomComponent,
+        component: UcTemplateComponent,
+        data: {
+          title: "Reimburse Request CNAF",
+          page: "ReimburseRequestMenuCNAFSelfCustomDEDI",
+        },
+      },
+      {
+        path: PathConstantSelfCustom.REIMB_APV_DETAIL_CNAF,
+        // component: EmployeeDetailSelfcustomComponent,
+        component: UcTemplateComponent,
+        data: {
+          title: "Reimburse Detail CNAF",
+          page: "ReimburseApprovalDetailMenuCNAFSelfCustomDEDI",
+        },
+      },
+      {
+        path: PathConstantSelfCustom.REIMB_APV_PAGING_CNAF,
+        // component: EmployeeDetailSelfcustomComponent,
+        component: UcTemplateComponent,
+        data: {
+          title: "Reimburse Paging CNAF",
+          page: "ReimburseApprovalPagingMenuCNAFSelfCustomDEDI",
+        },
+      },
+      {
+        path: PathConstantSelfCustom.RPT_EMP_CNAF,
+        // component: EmployeeDetailSelfcustomComponent,
+        component: UcTemplateComponent,
+        data: {
+          title: "Report Employee",
+          page: "ReportEmployeeCNAFSelfCustomDimas",
+        },
+      },
+      {
+        path: PathConstantSelfCustom.ASSET_TYPE_D_PAGING_CNAF,
+        // component: EmployeeDetailSelfcustomComponent,
+        component: UcTemplateComponent,
+        data: {
+          title: "Asset Type SelfCustom",
+          page: "AssetTypeSelfCustomDnew",
+        },
+      },
+      {
+        path: PathConstantSelfCustom.ASSET_TYPE_DETAIL,
+        // component: EmployeeDetailSelfcustomComponent,
+        component: UcTemplateComponent,
+        data: {
+          title: "Asset Type Detail SelfCustom",
+          page: "AssetTypeSelfCustomD",
+        },
+      },
+      {
+        path: PathConstantSelfCustom.TEST_DB_PAGING,
+        // component: EmployeeDetailSelfcustomComponent,
+        component: UcTemplateComponent,
+        data: {
+          title: "Tes DB",
+          page: "TestDBDnew",
+        },
+      },
+      {
+        path: PathConstantSelfCustom.TEST_DB_DETAIL,
+        // component: EmployeeDetailSelfcustomComponent,
+        component: UcTemplateComponent,
+        data: {
+          title: "Tes DB Detail",
+          page: "TestDBDnewDetail",
+        },
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class SelfcustomRoutingModule {}
