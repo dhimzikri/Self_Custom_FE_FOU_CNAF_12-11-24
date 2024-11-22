c2cce99260b83a747342be7b233d3cdc1daed130
Dimas Zikri <dschok182@gmail.com>
2024-11-22 15:58:43 +0700

    fixReimbPage

--- src/app/selfcustom/selfcustom-routing.module.ts
+++ src/app/selfcustom/selfcustom-routing.module.ts
@@ -32,7 +32,7 @@ const routes: Routes = [
         component: UcTemplateComponent,
         data: {
           title: "Reimburse Request CNAF",
-          page: "ReimburseRequestMenuCNAFSelfCustomDEDI",
+          page: "ReimburseRequestMenuCNAFSelfCustomDimas",
         },
       },
       {
